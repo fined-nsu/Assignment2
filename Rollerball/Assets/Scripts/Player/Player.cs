f8e2cb14b91c46f4811841b42aa3d5418ed8eadf
fined-nsu <71229695+fined-nsu@users.noreply.github.com>
2021-01-28 23:03:36 -0600

    Initial commit
    Completed rollerball tutorial.

---
+++ Rollerball/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int health = 3;
+
+    public event Action<Player> onPlayerDeath;
+
+    void collidedWithEnemy(Enemy enemy)
+    {
+        enemy.Attack(this);
+
+        if (health <= 0)
+        {
+            if (onPlayerDeath != null)
+            {
+                onPlayerDeath(this);
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Enemy enemy = col.collider.gameObject.GetComponent<Enemy>();
+        if (enemy)
+        {
+            collidedWithEnemy(enemy);
+        }
+    }
+
+}
